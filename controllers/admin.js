b2fc2effa44a7c03cfd13b5d29eddf881c0d634e
MuminovMuhammadqodir <muhammadqodirmuminov2@gmail.com>
2023-09-27 12:25:24 +0500

    fixed bugs

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 const Product = require('../models/product');
 const fileHalper = require('../util/file');
